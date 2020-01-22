8a2b79adb36b4f49fc7c4d534634581c37b6c9d3
Stephen Lin <linsteph@Stephens-MacBook-Pro-2.local>
2020-01-21 22:40:15 -0500

    Created Pokemon class and created routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PokemonComponent } from './pokemon/pokemon.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'pokemon', component: PokemonComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
