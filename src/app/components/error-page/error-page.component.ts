452132e6803d998fb3dfdbfa9ab49f161bd84797
Stephen Lin <stephenyhlin@hotmail.com>
2020-01-23 11:38:27 -0500

    Fixed routing issues and added error page

---
+++ src/app/components/error-page/error-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error-page',
+  templateUrl: './error-page.component.html',
+  styleUrls: ['./error-page.component.css']
+})
+export class ErrorPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  //Probably need to pass params to it lmao
+
+}
--- src/app/components/pokemon/pokemon.component.ts
+++ src/app/components/pokemon/pokemon.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Pokemon } from '../../Models/pokemon';
 import { Sprites } from '../../Models/sprites';
 import { PokemonService } from '../../services/pokemon.service';
+import { FormControl } from '@angular/forms';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pokemon',
@@ -9,19 +12,45 @@ import { PokemonService } from '../../services/pokemon.service';
   styleUrls: ['./pokemon.component.css']
 })
 export class PokemonComponent implements OnInit {
-  pokemon : Pokemon;
-  poke_id = 42;
+  pokemon: Pokemon;
+  search = new FormControl('');
+  poke_id: number;
+  poke_name: String;
 
   constructor(
-    private pokemonService : PokemonService,
+    private pokemonService: PokemonService,
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
-    this.getPokemon();
+    // this.getPokemon();
+    this.route.params.subscribe((params) => {
+      if (params['param']) {
+        // If it exists we will set it as our pokemon name. Pokemon name will always be the output
+        this.poke_name = params['param'];
+        this.poke_name = this.poke_name.split(' ').join('-');
+        this.search.setValue(this.poke_name);
+        this.getPokemonName();
+      }
+    })
   }
 
-  getPokemon(): void{
-    this.pokemonService.getPokemon(this.poke_id).subscribe(data => this.pokemon = data);
+  searchPoke(): void {
+    this.router.navigate(['/pokemon/', this.search.value]);
+  }
+
+  getPokemon(): void {
+    this.pokemonService.getPokemonId(this.poke_id).subscribe(data => this.pokemon = data);
+  }
+
+  getPokemonName(): void {
+    this.pokemonService.getPokemonName(this.poke_name.toLowerCase()).subscribe(data => this.pokemon = data,error =>{
+      if(error.status == 404){
+        this.router.navigate(['/error']);
+      }
+      
+    });
   }
 
 }
